d4b533435eae73ef5e40f5b44a40ac9028d0b318
jofwu <jofwu@users.noreply.github.com>
2020-02-26 13:45:40 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.8f40549d3ba70a56df900b85e36b861f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf3f4330d7fa722e752723c6ee6b970",
+    "url": "/reading-order/browserconfig.xml"
+  },
+  {
+    "revision": "fac4712182cb260c8694",
+    "url": "/reading-order/css/app.38c2de6b.css"
+  },
+  {
+    "revision": "5c323a0fa84094052a8046ca37be181c",
+    "url": "/reading-order/data.json"
+  },
+  {
+    "revision": "a50051a3215eb5194943e34babf32f5e",
+    "url": "/reading-order/img/cosmere.a50051a3.svg"
+  },
+  {
+    "revision": "248336d3618ba87913a7e8faddc611db",
+    "url": "/reading-order/index.html"
+  },
+  {
+    "revision": "fac4712182cb260c8694",
+    "url": "/reading-order/js/app.a030f34f.js"
+  },
+  {
+    "revision": "3c5dc707b78b9e896d86",
+    "url": "/reading-order/js/chunk-vendors.89e31642.js"
+  },
+  {
+    "revision": "abda5f466f677bb9dd0037c5b87acd28",
+    "url": "/reading-order/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/reading-order/robots.txt"
+  }
+]);
